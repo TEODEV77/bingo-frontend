5706f0f23bd3d9238bf3a460ee14181ecfcdec8e
TEODEV77 <mateo.orolot@gmail.com>
2024-11-25 13:47:15 -0500

    Starting project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'bingo-frontend';
+  username: string = '';
+  password: string = '';
+
+  onLogin() {
+    if (!this.username || !this.password) {
+      alert('Por favor, llena todos los campos');
+      return;
+    }
+  }
 }
